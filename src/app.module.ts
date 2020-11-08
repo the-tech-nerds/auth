f3b920ccb3f810fe714d5bb6e7d4a0b18b332ead
ARIFUL ISLAM <a.islam@stscapital.lt>
2020-11-08 13:46:30 +0600

    port change

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,11 +3,13 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
+import { AuthorizationModule } from './authorization/authorization.module';
 
 @Module({
   imports: [
     TypeOrmModule.forRoot(),
     UserModule,
+    AuthorizationModule,
   ],
   controllers: [AppController],
   providers: [AppService],
