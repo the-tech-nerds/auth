2ef571459410b4c1569493ce4e73716b21871b14
Junayed Rahimin <rahiminmyself@gmail.com>
2020-11-11 16:14:58 +0600

    user entity update

--- src/user/user.entity.ts
+++ src/user/user.entity.ts
@@ -1,15 +1,45 @@
 import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
 
+export type userType = "admin" | "user"
+
 @Entity()
 export class User {
   @PrimaryGeneratedColumn()
   id: number;
 
-  @Column()
+  @Column({
+    length: 100
+  })
   firstName: string;
 
-  @Column()
+  @Column({
+    length: 100
+  })
   lastName: string;
+
+  @Column({
+    length: 100
+  })
+  email: string;
+
+  @Column()
+  password: string;
+
+  @Column({nullable:true})
+  facebookAuth: string;
+
+  @Column({nullable:true})
+  googleAuth: string;
+
+  @Column({nullable:true})
+  imageUrl: string;
+
+  @Column({
+    type: "enum",
+    enum: ["admin", "user"],
+    default: "user"
+})
+role: userType
 
   @Column({ default: true })
   isActive: boolean;
