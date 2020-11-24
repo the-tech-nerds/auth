43374ed85c0fc44bb295264d80b98aae48a63325
rahimin <rahiminmyself@gmail.com>
2020-11-24 14:53:49 +0600

    integrated common service config. added auth service config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,7 @@
+import commonConfig from '@technerds/common-services/dist/config/common-config';
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
 import { RouterModule } from 'nest-router';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
@@ -8,10 +10,15 @@ import { AuthorizationModule } from './authorization/authorization.module';
 import { routes } from './route';
 import { AuthenticationModule } from './authentication/authentication.module';
 import { AddressModule } from './address/address.module';
+import configuration from './config/configuration';
 
 @Module({
   imports: [
     TypeOrmModule.forRoot(),
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [configuration, commonConfig],
+    }),
     RouterModule.forRoutes(routes),
     UserModule,
     AddressModule,
