e4ceb63beb85fcd8d5c9e8b13d6f27576d6f8340
Ariful Islam <arifulcuet52@gmail.com>
2021-03-08 01:25:33 +0600

    bug fixed

--- dist/notification/email/controllers/email.controller.d.ts
+++
@@ -1,8 +0,0 @@
-import { Response } from 'express';
-import { EMAILRequest } from '../requests/email.request';
-import { EmailService } from '../services/email.service';
-export declare class EmailController {
-    private readonly emailService;
-    constructor(emailService: EmailService);
-    sendEmail(email: EMAILRequest, res: Response): Promise<void>;
-}
